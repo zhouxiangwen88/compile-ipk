7a2434f4bf145a6979c9e08a3c9dcad0b57b8a94
123 <123@qq.com>
2024-10-08 21:17:31 +0800

    更新

--- .github/scripts/get_plugin_info.sh
+++ .github/scripts/get_plugin_info.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
 # 获取插件名称和版本
-NAME=$(grep -oP '(?<=NAME:=).*' Makefile)
-PKG_NAME=$(grep -oP '(?<=PKG_NAME:=).*' Makefile)
 
+# 读取Makefile文件
+MAKEFILE="./Makefile"
+
+# 提取NAME, PKG_NAME和PKG_VERSION
+NAME=$(grep -E '^NAME:=' $MAKEFILE | cut -d '=' -f 2)
+PKG_NAME=$(grep -E '^PKG_NAME:=' $MAKEFILE | cut -d '=' -f 2)
+PKG_VERSION=$(grep -E '^PKG_VERSION:=' $MAKEFILE | cut -d '=' -f 2)
 echo "插件名称: $NAME"
 echo "插件包名称: $PKG_NAME"
+echo "插件版本: $PKG_VERSION"
 
-if [[ "$PKG_NAME" == *'$(NAME)'* ]]; then
-  PLUGIN_NAME=$(echo "$PKG_NAME" | sed 's/\$\(NAME\)/'$NAME'/g')
-else
-  PLUGIN_NAME="$PKG_NAME"
-fi
-
-PLUGIN_VERSION=$(grep -oP '(?<=PKG_VERSION:=).*' Makefile)
-
-# 将变量添加到 GitHub Actions 环境变量
-echo "NAME=$NAME" >> $GITHUB_ENV
-echo "PKG_NAME=$PKG_NAME" >> $GITHUB_ENV
-echo "PLUGIN_NAME=$PLUGIN_NAME" >> $GITHUB_ENV
-echo "PLUGIN_VERSION=$PLUGIN_VERSION" >> $GITHUB_ENV
-
+# 替换PKG_NAME中的$(NAME)
+PLUGIN_NAME=${PKG_NAME//\$(NAME)/$NAME}
 echo "插件合并名称: $PLUGIN_NAME"
-echo "插件版本: $PLUGIN_VERSION"
+
+# 输出到$GITHUB_ENV
+echo "PLUGIN_NAME=$PLUGIN_NAME" >> $GITHUB_ENV
+echo "PLUGIN_VERSION=$PKG_VERSION" >> $GITHUB_ENV
